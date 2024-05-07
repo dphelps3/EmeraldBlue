6a0b742dcc08555f728d1140521cbfae29edfb33
David Phelps <DPhelps@syslogicinc.com>
2024-05-07 16:52:50 -0500

    Added PeopleData and PersonModel

---
+++ DataAccessLibrary/Models/PersonModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary.Models
+{
+    public class PersonModel
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string EmailAddress { get; set; } = string.Empty;
+    }
+}
--- DataAccessLibrary/SqlDataAccess.cs
+++ DataAccessLibrary/SqlDataAccess.cs
@@ -1,12 +1,48 @@
-﻿using System;
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using Microsoft.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataAccessLibrary
 {
-    public class SqlDataAccess
+    public class SqlDataAccess : ISqlDataAccess
     {
+        // shortcut: control + . to display dropdown to create field
+        private readonly IConfiguration _config;
+
+        public string ConnectionStringName { get; set; } = "Default";
+
+        public SqlDataAccess(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public async Task<List<T>> LoadData<T, U>(string sql, U parameters)
+        {
+            string connectionString = _config.GetConnectionString(ConnectionStringName);
+
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                // an Enumerable list of type T
+                var data = await connection.QueryAsync<T>(sql, parameters);
+
+                return data.ToList();
+            }
+        }
+
+        public async Task SaveData<T>(string sql, T parameters)
+        {
+            string connectionString = _config.GetConnectionString(ConnectionStringName);
+
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                await connection.ExecuteAsync(sql, parameters);
+            }
+        }
     }
 }
