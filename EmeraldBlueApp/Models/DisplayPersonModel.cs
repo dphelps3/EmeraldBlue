b58dda213dca25727a256dcf6b9ca1ecc94ffb50
David Phelps <DPhelps@syslogicinc.com>
2024-05-10 16:25:41 -0500

    Validation applied to model

---
+++ EmeraldBlueApp/Models/DisplayPersonModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmeraldBlueApp.Models
+{
+    public class DisplayPersonModel
+    {
+        [Required]
+        [StringLength(15, ErrorMessage = "First name is too long.")]
+        [MinLength(2, ErrorMessage = "First name cannot be smaller than 2 characters.")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(15, ErrorMessage = "Last name is too long.")]
+        [MinLength(2, ErrorMessage = "Last name cannot be smaller than 2 characters.")]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string EmailAddress { get; set; }
+    }
+}
